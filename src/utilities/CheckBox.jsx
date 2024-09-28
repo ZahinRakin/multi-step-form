9a2b4e307b657e07353c483cc59bf7dcaa70f3df
Zahin_Abdullah <zahinabdullahrakin@gmail.com>
2024-09-28 18:54:38 +0600

    there are some work left to do in this

--- src/utilities/CheckBox.jsx
+++ src/utilities/CheckBox.jsx
@@ -1,14 +1,28 @@
 import styles from "./CheckBox.module.css";
+import { useState, useEffect } from "react";
 
 function CheckBox(props){
+  const [checked, setChecked] = useState(false);
+
   function markSelected(identity){
+    setChecked(c => !c);
+
+    const inputElem = document.querySelector(`input[value="${props.id}"]`);
     const containerElem = document.querySelector(`.${identity}`).classList;
-    containerElem.toggle(styles.selected);
+    
+    inputElem.checked
+      ? containerElem.add(styles.selected)
+      : containerElem.remove(styles.selected);
   }
 
   return(
     <div className={`${styles.container} ${props.id}`}>
-      <input type="checkbox" value={props.id} onClick={() => markSelected(props.id)}/> {/*why should i name a input element */}
+      <input
+        type="checkbox" 
+        value={props.id} 
+        checked
+        onChange={() => markSelected(props.id)}/>
+
       <div className={styles[`sub-container`]}>
         <div className={`${styles.title} color-blue`}> 
           {props.title}
