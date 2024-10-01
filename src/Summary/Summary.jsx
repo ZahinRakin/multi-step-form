67955f353d3313b28bb83a2a0fe434e650da661e
Zahin_Abdullah <zahinabdullahrakin@gmail.com>
2024-10-01 19:29:59 +0600

    completed the summary part

--- src/Summary/Summary.jsx
+++ src/Summary/Summary.jsx
@@ -13,57 +13,59 @@ function Summary(){
   }, []);
 
   return(
-    <form id="form-part-4" className={styles["summary"]}>
-      <div className="container">
-        <h1 className="title">
-          Finishing up
-        </h1>
-        <h5 className="sub-title">
-          Double-check everything looks OK before confirming.
-        </h5>
-        <div className="services">
-          <div className="plan">
-            <div className="service-info">
-              <span>
-                Arcade(Monthly)
-              </span>
-
-              <Link to="/select-plan">
+    <form id="form-part-4" className={`${styles.summary} main-element`}>
+      <h1 className="title">
+        Finishing up
+      </h1>
+      <h5>
+        Double-check everything looks OK before confirming.
+      </h5>
+      
+      <div className={styles.services}>
+        <div className={`${styles.plan} ${styles[`grid-template`]}`}>
+          <div className={styles[`service-info`]}>
+            <span>
+              Arcade(Monthly)
+            </span>
+            <div>
+              <Link to="/select-plan" className={`${styles[`change-button`]}`}>
                 Change
               </Link>
             </div>
-            <div className="service-price">
-              $9/mo
+          </div>
+          <div className={`${styles[`service-price`]} ${styles[`price-portion`]}`}>
+            $9/mo
+          </div>
+        </div>
+        <div className={`${styles[`add-ons`]}`}>
+          <div className={`${styles[`add-on`]} ${styles[`grid-template`]}`}>
+            <div className={`${styles[`add-on-title`]} color-gray`}>
+              Online service
+            </div>
+            <div className={`${styles[`add-on-price`]} ${styles[`price-portion`]}`}>
+              +$1/mo
             </div>
           </div>
-          <div className="add-ons">
-            <div className="add-on">
-              <div className="title">
-                Online service
-              </div>
-              <div className="add-on-price">
-                +$1/mo
-              </div>
+          <div className={`${styles[`add-on`]} ${styles[`grid-template`]}`}>
+            <div className={`${styles[`add-on-title`]} color-gray`}>
+              Larger Storage
             </div>
-            <div className="add-on">
-              <div className="title">
-                Larger Storage
-              </div>
-              <div className="add-on-price">
-                +$2/mo
-              </div>
-            </div>
-          </div>
-          <div className="total-bill">
-            <div>
-              Total(per month)
-            </div>
-            <div>
-              +$12/mo
+            <div className={`${styles[`add-on-price`]} ${styles[`price-portion`]}`}>
+              +$2/mo
             </div>
           </div>
         </div>
+      </div>  
+
+      <div className={`${styles[`total-bill`]} ${styles[`grid-template`]}`}>
+        <div className="color-gray">
+          Total(per month)
+        </div>
+        <div className={`${styles[`total-price`]} ${styles[`price-portion`]}`}>
+          +$12/mo
+        </div>
       </div>
+
       <div className="buttons">
         <Link to="/add-ons" className="go-back-button">
           Go Back
